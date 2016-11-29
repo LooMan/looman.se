35d239ad969e23adc1d6acaa1f32ccd1d994e5d6
Fredrik Larsson <larsson.fredrik31@gmail.com>
2016-11-29 22:00:31 +0100

    added some logic in flow

--- app/flowField/app.js
+++ app/flowField/app.js
@@ -5,9 +5,23 @@ var cols, rows;
 var zoff = 0;
 
 var fr;
+var noiseSlider;
+var magSlider;
+var maxspeedSlider;
 
 var particles = [];
 var flowField;
+
+function reset() {
+    background(255)
+}
+
+var shouldShowArrows = false;
+
+function showArrows() {
+    shouldShowArrows = !shouldShowArrows;
+    reset();
+}
 
 function setup() {
     createCanvas(600, 600);
@@ -15,16 +29,29 @@ function setup() {
     rows = floor(height / scl);
 
     fr = createP('');
+    button = createButton('reset');
+    button.mousePressed(reset);
 
+    button = createButton('arrows');
+    button.mousePressed(showArrows);
+
+    noiseSlider = createSlider(1, 4, 4, 1);
+    magSlider = createSlider(0.1, 10, 0.5, 0.1);
+    maxspeedSlider = createSlider(1, 20, 4, 1);
     flowField = new Array(cols * rows);
 
     for (var i = 0; i < 300; i++) {
         particles[i] = new Particle();
     }
+
     background(255)
 }
 
 function draw() {
+    if(shouldShowArrows) {
+        background(255)
+    }
+
     var yoff = 0;
 
     for (var y = 0; y < rows; y++) {
@@ -32,24 +59,26 @@ function draw() {
 
         for (var x = 0; x < cols; x++) {
             var index = x + y * cols;
-            var a = noise(xoff, yoff, zoff) * TWO_PI * 4;
+            var a = noise(xoff, yoff, zoff) * TWO_PI * noiseSlider.value();
             var v = p5.Vector.fromAngle(a);
-            v.setMag(0.5);
+            v.setMag(magSlider.value());
             flowField[index] = v;
             xoff += inc;
 
-            // stroke(0, 100);
-            // strokeWeight(1);
-            // push();
-            // translate(x * scl, y * scl);
-            // rotate(v.heading());
-            // line(0, 0, scl, 0);
-            // pop();   
+            if (shouldShowArrows) {
+                stroke(0, 100);
+                strokeWeight(1);
+                push();
+                translate(x * scl, y * scl);
+                rotate(v.heading());
+                line(0, 0, scl, 0);
+                pop();   
+            }
         }
 
         yoff += inc;
 
-        zoff += 0.0005;        
+        zoff += 0.0008;        
     }
 
     for (var i = 0; i < particles.length; i++) {
@@ -66,13 +95,12 @@ function Particle() {
     this.pos = createVector(random(width), random(height));
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
-    this.maxSpeed = 4;
 
     this.prev = this.pos.copy();
 
     this.update = function() {
         this.vel.add(this.acc);
-        this.vel.limit(this.maxSpeed);
+        this.vel.limit(maxspeedSlider.value());
         this.pos.add(this.vel);
         this.acc.mult(0);
     }
